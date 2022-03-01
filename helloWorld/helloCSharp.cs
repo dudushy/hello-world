208118bebd921ab0f0b6db583b59650a4c9d8db3
Eduardo Talarico <eduardotalarico1337@gmail.com>
2022-03-01 00:17:33 -0300

    Update helloCSharp.cs

--- helloWorld/helloCSharp.cs
+++ helloWorld/helloCSharp.cs
@@ -1,1 +1,12 @@
-Console.WriteLine("Hello World");
+using System;
+
+namespace Hello
+{
+    internal static class World
+    {
+        static void Main()
+        {   
+            Console.WriteLine("Hello World");
+        }
+    }
+}
